c0e7dca983a34cb2dabd390ed421f1186e741044
Muhammad Dhiyaul Atha <mdhyaulatha@gmail.com>
2025-07-16 23:50:48 +0700

    fix: selesai perbaiki konflik dependensi dan sukses build

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/cv-muhammad-dhiyaul-atha.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
+import pdf from "../../Assets/cv-muhammad-dhiyaul-atha.pdf";
 
 function ResumeNew() {
   return (
@@ -17,29 +17,19 @@ function ResumeNew() {
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
-            <AiOutlineDownload /> &nbsp;Download CV
+            <AiOutlineDownload />
+            &nbsp;Download CV
           </Button>
         </Row>
 
-        <Row className="resume" style={{ justifyContent: "center", marginTop: "20px" }}>
+        <Row style={{ justifyContent: "center", marginTop: "20px" }}>
           <iframe
             src={pdf}
             width="80%"
             height="800px"
-            title="Resume"
             style={{ border: "none" }}
+            title="CV"
           ></iframe>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative", marginTop: "20px" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload /> &nbsp;Download CV
-          </Button>
         </Row>
       </Container>
     </div>
