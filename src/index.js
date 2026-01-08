cc3c94ab24a29aada67ec1abc52aa9fbd617eaf1
Muhammad Dhiyaul Atha <mdhyaulatha@gmail.com>
2026-01-09 01:51:49 +0700

    fix: Belajar lagi

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HelmetProvider } from "react-helmet-async";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
+      <SpeedInsights />
     </HelmetProvider>
   </React.StrictMode>
 );
